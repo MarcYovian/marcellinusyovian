77834d20582bb4fad266bd71b955fc46da12e435
marcellinus Yovian Indrastata <marcellinusyovian@gmail.com>
2024-02-01 10:44:19 +0700

    feat(home): create portfolio section but still a title, sub-heading and a short description of the portfolio section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -206,6 +206,19 @@
                             <h4 class="fill-current text-center text-base font-medium">Flutter</h4>
                         </div>
 
+                        {{-- Flutter --}}
+                        <div class="flex h-20 flex-col items-center justify-start p-2 text-white">
+                            <div class="flex h-12 w-9 items-center justify-center fill-current">
+                                <svg class="fill-current" role="img" viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <title>Python</title>
+                                    <path
+                                        d="M14.25.18l.9.2.73.26.59.3.45.32.34.34.25.34.16.33.1.3.04.26.02.2-.01.13V8.5l-.05.63-.13.55-.21.46-.26.38-.3.31-.33.25-.35.19-.35.14-.33.1-.3.07-.26.04-.21.02H8.77l-.69.05-.59.14-.5.22-.41.27-.33.32-.27.35-.2.36-.15.37-.1.35-.07.32-.04.27-.02.21v3.06H3.17l-.21-.03-.28-.07-.32-.12-.35-.18-.36-.26-.36-.36-.35-.46-.32-.59-.28-.73-.21-.88-.14-1.05-.05-1.23.06-1.22.16-1.04.24-.87.32-.71.36-.57.4-.44.42-.33.42-.24.4-.16.36-.1.32-.05.24-.01h.16l.06.01h8.16v-.83H6.18l-.01-2.75-.02-.37.05-.34.11-.31.17-.28.25-.26.31-.23.38-.2.44-.18.51-.15.58-.12.64-.1.71-.06.77-.04.84-.02 1.27.05zm-6.3 1.98l-.23.33-.08.41.08.41.23.34.33.22.41.09.41-.09.33-.22.23-.34.08-.41-.08-.41-.23-.33-.33-.22-.41-.09-.41.09zm13.09 3.95l.28.06.32.12.35.18.36.27.36.35.35.47.32.59.28.73.21.88.14 1.04.05 1.23-.06 1.23-.16 1.04-.24.86-.32.71-.36.57-.4.45-.42.33-.42.24-.4.16-.36.09-.32.05-.24.02-.16-.01h-8.22v.82h5.84l.01 2.76.02.36-.05.34-.11.31-.17.29-.25.25-.31.24-.38.2-.44.17-.51.15-.58.13-.64.09-.71.07-.77.04-.84.01-1.27-.04-1.07-.14-.9-.2-.73-.25-.59-.3-.45-.33-.34-.34-.25-.34-.16-.33-.1-.3-.04-.25-.02-.2.01-.13v-5.34l.05-.64.13-.54.21-.46.26-.38.3-.32.33-.24.35-.2.35-.14.33-.1.3-.06.26-.04.21-.02.13-.01h5.84l.69-.05.59-.14.5-.21.41-.28.33-.32.27-.35.2-.36.15-.36.1-.35.07-.32.04-.28.02-.21V6.07h2.09l.14.01zm-6.47 14.25l-.23.33-.08.41.08.41.23.33.33.23.41.08.41-.08.33-.23.23-.33.08-.41-.08-.41-.23-.33-.33-.23-.41-.08-.41.08z" />
+                                </svg>
+                            </div>
+                            <h4 class="fill-current text-center text-base font-medium">Python</h4>
+                        </div>
+
                     </div>
                 </div>
             </div>
@@ -218,8 +231,11 @@
         <div class="container">
             <div class="w-full px-4">
                 <div class="mx-auto mb-16 max-w-xl text-center">
-                    <h3 class="text-base font-semibold text-white xl:text-2xl">What Am I Providing</h3>
-                    <h1 class="text-4xl font-bold text-primary xl:text-6xl">SERVICES</h1>
+                    <h3 class="text-base font-semibold text-white md:text-lg xl:text-2xl">What Am I Providing</h3>
+                    <h1 class="text-4xl font-bold text-primary md:text-5xl xl:text-6xl mb-2 xl:mb-4">SERVICES</h1>
+                    <p class="mb-8 text-xs font-medium leading-relaxed md:text-sm text-third">Elevate your digital
+                        presence with my comprehensive web and mobile development services, tailored to bring your ideas
+                        to life and enhance user engagement.</p>
                 </div>
             </div>
 
@@ -252,6 +268,34 @@
         </div>
     </section>
     {{-- Service Section End --}}
+
+    {{-- Portfolio Section Start --}}
+    <section id="portfolio" class="bg-dark-semi pt-36 pb-16">
+        <div class="container">
+            <div class="w-full px-4">
+                <div class="mx-auto mb-16 max-w-xl text-center">
+                    <h3 class="text-base font-semibold text-white md:text-lg xl:text-2xl">Diverse Web and Mobile
+                        Creations</h3>
+                    <h1 class="text-4xl font-bold text-primary md:text-5xl xl:text-6xl mb-2 xl:mb-4">PROJECTS SHOWCASE
+                    </h1>
+                    <p class="mb-8 text-xs font-medium leading-relaxed md:text-sm text-third">Explore my diverse
+                        portfolio
+                        featuring web
+                        and mobile projects, each a testament to my commitment
+                        to innovation and seamless user experiences.</p>
+                </div>
+            </div>
+
+            <div class="w-full px-4">
+                <div class="grid grid-cols-1">
+                    <div class="bg-third">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    {{-- Portfolio Section End --}}
 
     <script src="https://kit.fontawesome.com/aa3910db9c.js" crossorigin="anonymous"></script>
 </body>
